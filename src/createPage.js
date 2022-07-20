b8e5fef7dd3d93de31befdd2c385028695787d34
Josh Hartwig <joshman211@gmail.com>
2022-07-20 17:00:22 -0500

    added content to about and contact

--- src/createPage.js
+++ src/createPage.js
@@ -86,7 +86,12 @@ export function setMainPage(section){
         let main = document.querySelector("#main");
         main.innerHTML = 
         `
-        <div>Hello from About</div>  
+        <p class="rs-about-title">
+        About Us
+      </p>
+      <p class="rs-about">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p> 
         `;
     }
 
@@ -94,7 +99,10 @@ export function setMainPage(section){
         let main = document.querySelector("#main");
         main.innerHTML = 
         `
-        <div>Hello from Contact</div>  
+        <p class="rs-contact-para">
+        We are always looking to see new faces. Below is our address, stop by and come see us.
+      </p>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3337.9297346996223!2d-96.68222488404561!3d33.21593828084164!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x864c15ea11036065%3A0x206f422a342e6797!2sJersey%20Mike&#39;s%20Subs!5e0!3m2!1sen!2sus!4v1658347107091!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         `;
     }
 
